3c1e8ba8286b1317418793ef972eab6393481282
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-01-09 19:43:59 -0500

    chore(app): Remove js files from the repo

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import { HelloWorldDirective } from './hello-world/hello-world';
-var app;
-(function (app) {
-    angular
-        .module('app', [])
-        .directive('helloWorld', HelloWorldDirective.instance);
-})(app || (app = {}));
